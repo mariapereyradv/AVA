a8ab3d94dd96d5c05ea58d19d5c23256345de8fb
mariap <maria.pereyra@davinci.edu.ar>
2025-09-19 20:09:42 -0300

    feat: subo una clase app para que funcione la logica del proyecto

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-import Tablero from '../components/tablero.js';
-
+import Tablero from './components/tablero.js';
+import Portada from './components/portada.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // creo una instancia del controlador principal.
-    const tableroDeVuelos = new Tablero();
-    
-    // llamo un método que inicia toda la lógica.
-    tableroDeVuelos.iniciar();
+  const portada = new Portada();
+  portada.iniciar();
+
+  const tableroDeVuelos = new Tablero();
+  tableroDeVuelos.iniciar();
 });
